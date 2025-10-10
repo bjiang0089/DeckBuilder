12d8beec1928e9962f06d0c2ef29d3abb66af202
bjiang0089 <bjiang0089@gmail.com>
2025-10-10 19:20:19 -0400

    Refactored sections of code into methods for cleanliness
    Updated Readme

--- src/main/java/APITutorial.java
+++ src/main/java/APITutorial.java
@@ -7,30 +7,82 @@ import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.Scanner;
 
 public class APITutorial {
 
     public static void main(String[] args) {
         Gson gson = new Gson();
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("\n\n\n");
+        System.out.println("Enter Scryfall name search");
+        System.out.println("Do not use additional search parameters like c, o, mv, etc");
+        System.out.print("> ");
 
         // Construct Scryfall search GET request
+        HttpRequest getRequest = nameSearch(scan.nextLine());
+        scan.close();
+
+        // Construct the HTTP Client to send the request
+        HttpClient client = HttpClient.newHttpClient();
+        HttpResponse<String> response = sendSearch(client, getRequest);
+
+        // Print the name of the cards found
+        List<Card> cards = extractData(response, gson);
+        if (cards != null) {
+            for (Card c : cards) {
+                System.out.println(c.getName());
+            }
+        }
+
+        System.out.println("\n\n\nEnd of program\n\n\n");
+    }
+
+    /**
+     * Create a search request. Only searches by name
+     * Do not use any other search paramaters like mv= or o:
+     * @param s the name to search
+     * @return HTTP Request searching for the name
+     */
+    public static HttpRequest nameSearch(String s) {
         HttpRequest getReqest = null;
+        StringBuilder str = new StringBuilder(Constants.URL + "/cards/search?q=");
+        char[] query = s.toCharArray();
+
+        // Replace all spaces with '+'
+        for (char c: query) {
+            if (c == ' ') {
+                str.append('+');
+            } else {
+                str.append(c);
+            }
+        }
+
         try {
             getReqest = HttpRequest.newBuilder()
                     // Search for all cards that have "finale" in its name
-                    .uri(new URI(Constants.URL + "/cards/search?q=finale"))
+                    .uri(new URI(str.toString()))
                     .header("User-Agent", "DeckBuilder/1.0")
                     .header("Accept", "*/*")
                     .build();
         } catch (URISyntaxException e) {
             System.err.println("Invalid URI address");
+            return null;
         }
+        return getReqest;
+    }
 
-        // Construct the HTTP Client to send the request
-        HttpClient client = HttpClient.newHttpClient();
+    /**
+     * Send a search query as a GET request
+     * @param client HTTPClient to send requests
+     * @param req the request to send
+     * @return The response to the HTTP GET Request
+     */
+    public static HttpResponse<String> sendSearch(HttpClient client, HttpRequest req) {
         HttpResponse<String> response = null;
         try {
-            response = client.send(getReqest, HttpResponse.BodyHandlers.ofString());
+            response = client.send(req, HttpResponse.BodyHandlers.ofString());
         } catch (IOException e) {
             System.err.println("IO Error Occured");
         } catch (InterruptedException e) {
@@ -39,26 +91,35 @@ public class APITutorial {
             System.err.println("Exception Thrown.");
         }
 
-        System.out.println("Http Response Received");
+        return response;
+    }
+
+    /**
+     * Extract the list of cards found in the search
+     * @param response HTTP response to parse
+     * @param gson GSON object to parse JSON to SearchResponse
+     * @return List of Cards or null to indicate an error
+     */
+    public static List<Card> extractData(HttpResponse<String> response, Gson gson) {
         try {
             System.out.printf("Response Status: %d\n", response.statusCode());
 //            System.out.printf("\n\n%s", response.body());
+            if (response.statusCode() != 200) {
+                return null;
+            }
         } catch (Exception e) {
             System.err.println("Unable to get Status Code");
+            return null;
         }
         SearchResponse searchResult = null;
         try {
             searchResult = gson.fromJson(response.body(), SearchResponse.class);
-            System.out.printf("Cards returned: %d\n", searchResult.getData().size());
+            System.out.printf("Cards returned: %d\n\n", searchResult.getData().size());
         } catch (Exception e) {
             System.err.println("Error converting JSon to Search Response");
+            return null;
         }
 
-        List<Card> cards = searchResult.getData();
-        for (Card c : cards) {
-            System.out.println(c.getName());
-        }
-
-        System.out.println("\n\n\nEnd of program\n\n\n");
+        return searchResult.getData();
     }
 }
